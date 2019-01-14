df11e080821a6267e269b7ab7fca121e2429675d
xiedacon <xiedacon@qq.com>
2019-01-14 11:27:17 +0800

    feat: json.parse

--- lib/pretty/json.lua
+++ lib/pretty/json.lua
@@ -1,24 +1,34 @@
 -- Copyright (c) 2018, Souche Inc.
 
+local Constant = require "pretty.json.constant"
 local Serializer = require "pretty.json.serializer"
+local Parser = require "pretty.json.parser"
 
-local json = {}
+local json = {
+    null = Constant.NULL
+}
 
 function json.stringify(obj, replacer, space, print_address)
     if type(space) ~= "number" then space = 0 end
-    local fragments = {}
 
-    Serializer({
+    return Serializer({
         print_address = print_address,
         stream = {
+            fragments = {},
             write = function(self, ...)
                 for i = 1, #{...} do
-                    fragments[#fragments + 1] = ({...})[i]
+                    self.fragments[#self.fragments + 1] = ({...})[i]
                 end
+            end,
+            toString = function(self)
+                return table.concat(self.fragments)
             end
         }
-    }):json(obj, replacer, space, space)
-    return table.concat(fragments)
+    }):json(obj, replacer, space, space):toString()
+end
+
+function json.parse(str, without_null)
+    return Parser():json(str, 1, without_null)
 end
 
 return json
--- lib/pretty/json/serializer.lua
+++ lib/pretty/json/serializer.lua
@@ -1,20 +1,31 @@
 -- Copyright (c) 2018, Souche Inc.
 
+local Constant = require "pretty.json.constant"
+
+local NULL = Constant.NULL
+local ESC_MAP = Constant.ESC_MAP
+
 local function kind_of(obj)
-    if type(obj) ~= 'table' then return type(obj) end
+    if type(obj) ~= "table" then return type(obj) end
+    if obj == NULL then return "nil" end
+
     local i = 1
     for _ in pairs(obj) do
-        if obj[i] ~= nil then i = i + 1 else return 'table' end
+        if obj[i] ~= nil then i = i + 1 else return "table" end
     end
-    if i == 1 then return 'table' else return 'array' end
+
+    if i == 1 then
+        return "table"
+    else
+        return "array"
+    end
 end
 
 local function escape_str(s)
-    local in_char  = {'\\', '"', '/', '\b', '\f', '\n', '\r', '\t'}
-    local out_char = {'\\', '"', '/',  'b',  'f',  'n',  'r',  't'}
-    for i, c in ipairs(in_char) do
-        s = s:gsub(c, '\\' .. out_char[i])
+    for k, v in pairs(ESC_MAP) do
+        s = s:gsub(k, v)
     end
+
     return s
 end
 
@@ -41,71 +52,79 @@ setmetatable(Serializer, {
 function Serializer:space(n)
     local stream = self.stream
     for i = 1, n or 0 do
-        stream:write(' ')
+        stream:write(" ")
     end
+
+    return self
 end
 
 function Serializer:key(key)
     local stream = self.stream
     local kind = kind_of(key)
 
-    if kind == 'array' then
-        error('Can\'t encode array as key.')
-    elseif kind == 'table' then
-        error('Can\'t encode table as key.')
-    elseif kind == 'string' then
-        stream:write('"', escape_str(key), '"')
-    elseif kind == 'number' then
-        stream:write('"', tostring(key), '"')
+    if kind == "array" then
+        error("Can't encode array as key.")
+    elseif kind == "table" then
+        error("Can't encode table as key.")
+    elseif kind == "string" then
+        stream:write("\"", escape_str(key), "\"")
+    elseif kind == "number" then
+        stream:write("\"", tostring(key), "\"")
     elseif self.print_address then
         stream:write(tostring(key))
     else
-        error('Unjsonifiable type: ' .. kind .. '.')
+        error("Unjsonifiable type: " .. kind .. ".")
     end
+
+    return self
 end
 
 function Serializer:array(arr, replacer, indent, space)
     local stream = self.stream
 
-    stream:write('[')
+    stream:write("[")
     for i, v in ipairs(arr) do
         if replacer then v = replacer(k, v) end
 
-        stream:write(i == 1 and '' or ',')
-        stream:write(space > 0 and '\n' or '')
+        stream:write(i == 1 and "" or ",")
+        stream:write(space > 0 and "\n" or "")
         self:space(indent)
         self:json(v, replacer, indent + space, space)
     end
     if #arr > 0 then
-        stream:write(space > 0 and '\n' or '')
+        stream:write(space > 0 and "\n" or "")
         self:space(indent - space)
     end
-    stream:write(']')
+    stream:write("]")
+
+    return self
 end
 
 function Serializer:table(obj, replacer, indent, space)
     local stream = self.stream
 
-    stream:write('{')
+    stream:write("{")
     local len = 0
     for k, v in pairs(obj) do
         if replacer then v = replacer(k, v) end
 
         if v ~= nil then
-            stream:write(len == 0 and '' or ',')
-            stream:write(space > 0 and '\n' or '')
+            stream:write(len == 0 and "" or ",")
+            stream:write(space > 0 and "\n" or "")
             self:space(indent)
             self:key(k)
-            stream:write(space > 0 and ': ' or ':')
+            stream:write(space > 0 and ": " or ":")
             self:json(v, replacer, indent + space, space)
             len = len + 1
         end
     end
     if len > 0 then
-        stream:write(space > 0 and '\n' or '')
+        stream:write(space > 0 and "\n" or "")
         self:space(indent - space)
     end
-    stream:write('}')
+    stream:write("}")
+
+    return self
 end
 
 function Serializer:json(obj, replacer, indent, space)
@@ -113,25 +132,27 @@ function Serializer:json(obj, replacer, indent, space)
     local kind = kind_of(obj)
 
     self.depth = self.depth + 1
-    if self.depth > self.max_depth then error('Reach max depth: ' .. tostring(self.max_depth)) end
+    if self.depth > self.max_depth then error("Reach max depth: " .. tostring(self.max_depth)) end
 
-    if kind == 'array' then
+    if kind == "array" then
         self:array(obj, replacer, indent, space)
-    elseif kind == 'table' then
+    elseif kind == "table" then
         self:table(obj, replacer, indent, space)
-    elseif kind == 'string' then
-        stream:write('"', escape_str(obj), '"')
-    elseif kind == 'number' then
+    elseif kind == "string" then
+        stream:write("\"", escape_str(obj), "\"")
+    elseif kind == "number" then
         stream:write(tostring(obj))
-    elseif kind == 'boolean' then
+    elseif kind == "boolean" then
         stream:write(tostring(obj))
-    elseif kind == 'nil' then
-        stream:write('null')
+    elseif kind == "nil" then
+        stream:write("null")
     elseif self.print_address then
         stream:write(tostring(obj))
     else
-        error('Unjsonifiable type: ' .. kind)
+        error("Unjsonifiable type: " .. kind)
     end
+
+    return self
 end
 
 function Serializer:toString()
